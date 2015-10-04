06bc0cdd827ea1da81f1047803ab04038b01d389
Jared Parsons <jaredpar@microsoft.com>
2015-10-03 17:10:20 -0700

    Initial website

---
+++ Dashboard/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Dashboard.Startup))]
+namespace Dashboard
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
