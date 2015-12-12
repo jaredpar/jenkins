08f59c9dcf0fb4d6948ede3d185a941385174416
Jared Parsons <jaredpar@microsoft.com>
2015-12-11 16:55:31 -0800

    Start of Jenkins work

---
+++ Dashboard/Controllers/JenkinsController.cs
@@ -0,0 +1,43 @@
+﻿using Dashboard.Models;
+using Roslyn.Jenkins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Dashboard.Controllers
+{
+    public class JenkinsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View(GetAllJobs());
+        }
+
+        public ActionResult Job(string name)
+        {
+            if (name == null)
+            {
+                return View(viewName: "Index", model: GetAllJobs());
+            }
+            return View();
+        }
+
+        private static AllJobsModel GetAllJobs()
+        {
+            var model = new AllJobsModel();
+            var client = CreateClient();
+            foreach (var name in client.GetJobNames())
+            {
+                model.Names.Add(name);
+            }
+            return model;
+        }
+
+        private static RoslynClient CreateClient()
+        {
+            return new RoslynClient();
+        }
+    }
+}
