b53dac9db7fd6cc5e5f9db6655bfe6396b2cfe08
Jared Parsons <jaredpar@microsoft.com>
2016-04-12 16:29:06 -0700

    Fixed the schedule time

--- JenkinsJobs/Functions.cs
+++ JenkinsJobs/Functions.cs
@@ -5,16 +5,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage;
+using Roslyn;
+using Roslyn.Azure;
+using Roslyn.Jenkins;
 
 namespace JenkinsJobs
 {
     public class Functions
     {
-        // This function will get triggered/executed when a new message is written 
-        // on an Azure Queue called queue.
-        public static void ProcessQueueMessage([QueueTrigger("queue")] string message, TextWriter log)
+        public static async Task PopulateBuildTables(TextWriter logger)
         {
-            log.WriteLine(message);
+            try
+            {
+                var connectionString = CloudConfigurationManager.GetSetting(SharedConstants.StorageConnectionStringName);
+                var storageAccount = CloudStorageAccount.Parse(connectionString);
+                var tableClient = storageAccount.CreateCloudTableClient();
+                var buildFailureTable = tableClient.GetTableReference(AzureConstants.TableNameBuildFailure);
+                buildFailureTable.CreateIfNotExists();
+                var buildProcessedTable = tableClient.GetTableReference(AzureConstants.TableNameBuildProcessed);
+                buildProcessedTable.CreateIfNotExists();
+
+                // TODO: Need a Jenkins token as well to be able to query our non-public jobs.
+                var githubConnectionString = CloudConfigurationManager.GetSetting(SharedConstants.GithubConnectionStringName);
+                var roslynClient = string.IsNullOrEmpty(githubConnectionString)
+                    ? new RoslynClient()
+                    : new RoslynClient(connectionString: githubConnectionString);
+
+                var util = new JobTableUtil(buildProcessedTable: buildProcessedTable, buildFailureTable: buildFailureTable, roslynClient: roslynClient, textWriter: logger);
+                await util.MoveUnknownToIgnored();
+                await util.Populate();
+            }
+            catch (Exception ex)
+            {
+                logger.WriteLine($"Unable to run the local request: {ex}");
+            }
         }
     }
 }
--- JenkinsJobs/Program.cs
+++ JenkinsJobs/Program.cs
@@ -1,14 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
-using Microsoft.WindowsAzure;
-using Microsoft.WindowsAzure.Storage;
-using Roslyn.Jenkins;
-using Roslyn.Azure;
-using Roslyn;
 
 namespace JenkinsJobs
 {
@@ -18,40 +9,11 @@ namespace JenkinsJobs
         // AzureWebJobsDashboard and AzureWebJobsStorage
         public static void Main()
         {
+            Functions.PopulateBuildTables(Console.Out).Wait();
             /*
             var host = new JobHost();
-            // The following code ensures that the WebJob will be running continuously
             host.RunAndBlock();
             */
-            LocalRun().Wait();
-        }
-
-        private static async Task LocalRun()
-        {
-            try
-            {
-                var connectionString = CloudConfigurationManager.GetSetting(SharedConstants.StorageConnectionStringName);
-                var storageAccount = CloudStorageAccount.Parse(connectionString);
-                var tableClient = storageAccount.CreateCloudTableClient();
-                var buildFailureTable = tableClient.GetTableReference(AzureConstants.TableNameBuildFailure);
-                buildFailureTable.CreateIfNotExists();
-                var buildProcessedTable = tableClient.GetTableReference(AzureConstants.TableNameBuildProcessed);
-                buildProcessedTable.CreateIfNotExists();
-
-                // TODO: Need a Jenkins token as well to be able to query our non-public jobs.
-                var githubConnectionString = CloudConfigurationManager.GetSetting(SharedConstants.GithubConnectionStringName);
-                var roslynClient = string.IsNullOrEmpty(githubConnectionString)
-                    ? new RoslynClient()
-                    : new RoslynClient(connectionString: githubConnectionString);
-
-                var util = new JobTableUtil(buildProcessedTable: buildProcessedTable, buildFailureTable: buildFailureTable, roslynClient: roslynClient, textWriter: Console.Out);
-                await util.MoveUnknownToIgnored();
-                await util.Populate();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Unable to run the local request: {ex}");
-            }
         }
     }
 }
