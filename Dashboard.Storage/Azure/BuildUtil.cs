47b2dbb8abea79212f54db12fce4fe084ec00d2f
Daniel Podder <dapodd@microsoft.com>
2016-06-13 17:58:23 -0500

    Remove redundant GetBuildResults controller

--- Dashboard.Storage/Azure/BuildUtil.cs
+++ Dashboard.Storage/Azure/BuildUtil.cs
@@ -41,16 +41,6 @@ namespace Dashboard.Azure
             return _buildResultDateTable.ExecuteQuery(query).ToList();
         }
 
-        public List<BuildResultEntity> GetBuildResults(DateTimeOffset startDate, ClassificationKind kind, string viewName)
-        {
-            var filter = FilterUtil
-                .SinceDate(ColumnNames.PartitionKey, startDate)
-                .And(FilterUtil.Column(nameof(BuildResultEntity.ClassificationKindRaw), kind.ToString()));
-            filter = FilterView(filter, viewName);
-            var query = new TableQuery<BuildResultEntity>().Where(filter);
-            return _buildResultDateTable.ExecuteQuery(query).ToList();
-        }
-
         public List<BuildResultEntity> GetBuildResultsByKindName(DateTimeOffset startDate, string kindName, string viewName)
         {
             var filter = FilterUtil
