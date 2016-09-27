c948cb4ee2e80dae9d7c12b79705dcc454968173
Jared Parsons <jaredpar@microsoft.com>
2016-09-27 09:25:39 -0400

    Added DateTimeKey
    Replacement for DateTime.  It's a type that can represent dates and times
    in a manner that's good for Azure table storage keys. In particular it's
    much more human readable than the current one.

---
+++ Dashboard.Storage/Azure/DateTimeKey.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dashboard.Azure
+{
+    [Flags]
+    public enum DateTimeKeyFlags
+    {
+        Default = Date,
+        DateTime = Date | Time,
+
+        Date = 0x001,
+        Time = 0x002,
+    }
+
+    /// <summary>
+    /// Uses a date as a partition key.  The format of the key is intended to create a 
+    /// range partition by using increasing values.
+    /// </summary>
+    public struct DateTimeKey : IEquatable<DateTimeKey>
+    {
+        public string Key { get; }
+        public DateTimeKeyFlags Flags { get; }
+
+        public DateTimeKey(DateTimeOffset dateTime, DateTimeKeyFlags flags)
+        {
+            Key = CreateKey(dateTime, flags);
+            Flags = flags;
+        }
+
+        public static string CreateKey(DateTimeOffset dateTime, DateTimeKeyFlags flags)
+        {
+            var builder = new StringBuilder();
+            if (DateTimeKeyFlags.Date == (flags & DateTimeKeyFlags.Date))
+            {
+                builder.Append(dateTime.Year.ToString("0000"));
+                builder.Append(dateTime.Month.ToString("00"));
+                builder.Append(dateTime.Day.ToString("00"));
+            }
+
+            if (DateTimeKeyFlags.Time == (flags & DateTimeKeyFlags.Time))
+            {
+                builder.Append("T");
+                builder.Append(dateTime.Hour.ToString("00"));
+                builder.Append(dateTime.Minute.ToString("00"));
+            }
+
+            return builder.ToString();
+        }
+
+        public static bool operator==(DateTimeKey left, DateTimeKey right) => left.Key == right.Key;
+        public static bool operator!=(DateTimeKey left, DateTimeKey right) => !(left == right);
+        public bool Equals(DateTimeKey other) => this == other;
+        public override bool Equals(object obj) => obj is DateTimeKey && Equals((DateTimeKey)obj);
+        public override int GetHashCode() => Key.GetHashCode();
+        public override string ToString() => Key;
+    }
+}
---
+++ Dashboard.Tests/DateTimeKeyTests.cs
@@ -0,0 +1,122 @@
+﻿using Dashboard.Azure;
+using Dashboard.Jenkins.Tests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Dashboard.Tests
+{
+    public class DateTimeKeyTests
+    {
+        public sealed class EqualityTests : DateTimeKeyTests
+        {
+            [Fact]
+            public void Date()
+            {
+                var date = new DateTimeOffset(year: 2016, month: 4, day: 1, hour: 1, minute: 0, second: 0, offset: TimeSpan.Zero);
+                EqualityUnit
+                    .Create(new DateTimeKey(date, DateTimeKeyFlags.Date))
+                    .WithEqualValues(
+                        new DateTimeKey(date, DateTimeKeyFlags.Date),
+                        new DateTimeKey(date.AddHours(1), DateTimeKeyFlags.Date))
+                    .WithNotEqualValues(
+                        new DateTimeKey(date, DateTimeKeyFlags.Time),
+                        new DateTimeKey(date.AddDays(1), DateTimeKeyFlags.Date))
+                    .RunAll(
+                        (x, y) => x == y,
+                        (x, y) => x != y);
+            }
+
+            [Fact]
+            public void Time()
+            {
+                var date = new DateTimeOffset(year: 2016, month: 4, day: 1, hour: 1, minute: 0, second: 0, offset: TimeSpan.Zero);
+                EqualityUnit
+                    .Create(new DateTimeKey(date, DateTimeKeyFlags.Time))
+                    .WithEqualValues(
+                        new DateTimeKey(date, DateTimeKeyFlags.Time),
+                        new DateTimeKey(date.AddDays(1), DateTimeKeyFlags.Time))
+                    .WithNotEqualValues(
+                        new DateTimeKey(date, DateTimeKeyFlags.Date),
+                        new DateTimeKey(date.AddHours(1), DateTimeKeyFlags.Time))
+                    .RunAll(
+                        (x, y) => x == y,
+                        (x, y) => x != y);
+            }
+        }
+
+        public sealed class KeyTests
+        {
+            [Fact]
+            public void DateSimple()
+            {
+                var date = DateTimeOffset.Parse("2016/09/15");
+                var key = new DateTimeKey(date, DateTimeKeyFlags.Date);
+                Assert.Equal("20160915", key.Key);
+            }
+
+            [Fact]
+            public void DateTimeSimple()
+            {
+                var date = DateTimeOffset.Parse("2016/09/15 1:00AM");
+                var key = new DateTimeKey(date, DateTimeKeyFlags.DateTime);
+                Assert.Equal("20160915T0100", key.Key);
+            }
+
+            [Fact]
+            public void DateTimeWidth()
+            {
+                var date = DateTimeOffset.Parse("2016/1/2 1:01PM");
+                var key = new DateTimeKey(date, DateTimeKeyFlags.DateTime);
+                Assert.Equal("20160102T1301", key.Key);
+            }
+
+            [Fact]
+            public void DateTimeMilitary()
+            {
+                var date = DateTimeOffset.Parse("2016/09/15 1:00PM");
+                var key = new DateTimeKey(date, DateTimeKeyFlags.DateTime);
+                Assert.Equal("20160915T1300", key.Key);
+            }
+
+            [Fact]
+            public void DateComparison()
+            {
+                var date = DateTimeOffset.Parse("2016/09/15 1:00PM");
+                var key = new DateTimeKey(date, DateTimeKeyFlags.Date);
+                for (var i = 1; i < 100; i++)
+                {
+                    var newKey = new DateTimeKey(date.AddDays(i), key.Flags);
+                    Assert.True(string.CompareOrdinal(newKey.Key, key.Key) > 0);
+                }
+            }
+
+            [Fact]
+            public void DateTimeComparisonHours()
+            {
+                var date = DateTimeOffset.Parse("2016/09/15 1:00PM");
+                var key = new DateTimeKey(date, DateTimeKeyFlags.DateTime);
+                for (var i = 1; i < 100; i++)
+                {
+                    var newKey = new DateTimeKey(date.AddHours(i), key.Flags);
+                    Assert.True(string.CompareOrdinal(newKey.Key, key.Key) > 0);
+                }
+            }
+
+            [Fact]
+            public void DateTimeComparisonMinutes()
+            {
+                var date = DateTimeOffset.Parse("2016/09/15 1:00PM");
+                var key = new DateTimeKey(date, DateTimeKeyFlags.DateTime);
+                for (var i = 1; i < 100; i++)
+                {
+                    var newKey = new DateTimeKey(date.AddMinutes(i), key.Flags);
+                    Assert.True(string.CompareOrdinal(newKey.Key, key.Key) > 0);
+                }
+            }
+        }
+    }
+}
